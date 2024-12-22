09ef543181eaf3ae95fbd11735753f1d5466511b
Alberto Alas <oa3973392@gmail.com>
2024-12-21 18:43:09 -0600

    users with roles added

--- src/controllers/dataController.js
+++ src/controllers/dataController.js
@@ -1,6 +1,8 @@
 const DataUsers = require('../models/UserSchema')
 const { generateToken } = require('../utils/jwtUtils')
 const TokenSchema = require('../models/TokenSchema')
+const DataTeachers = require('../models/TeacherSchema')
+const DataStudents = require('../models/StudentSchema')
 
 //controllers for DataUsers
 
@@ -18,20 +20,45 @@ exports.listUsers = async (req, res, next) => {
 
 //create new user
 exports.createUser = async (req, res, next) => {
-    const { user, email, password, rol } = req.body
-    const data = new DataUsers({
-        user: user || 'No user',
-        email: email || 'No email',
-        password: password || 'No password',
-        rol: rol || 'No rol'
-    })
+    const { user, email, password, role, phoneNumber, address, carnet } = req.body
+    let newUser;
+
+    if (role === 'student') {
+        newUser = new DataStudents({
+            user: user || 'no user',
+            email: email || 'no email',
+            password: password || 'no password',
+            phoneNumber: phoneNumber || 'no phoneNumber',
+            address: address || 'no address',
+        })
+    } else if (role === 'teacher') {
+        newUser = new DataTeachers({
+            user: user || 'no user',
+            email: email || 'no email',
+            password: password || 'no password',
+            phoneNumber: phoneNumber || 'no phoneNumber',
+            address: address || 'no address',
+            carnet: carnet || 'no carnet'
+        })
+    } else {
+        newUser = new DataUsers({
+            user: user || 'no user',
+            email: email || 'no email',
+            password: password || 'no password',
+            role: role || 'no role'
+        })
+    }
+
     try {
         const existEmail = await DataUsers.findOne({ email: email })
+        const exitsUser = await DataUsers.findOne({ user: user })
         if (existEmail !== null) {
             return res.status(400).json({ message: 'Email is already used' })
+        } else if (exitsUser !== null) {
+            return res.status(400).json({ message: 'User name is already used' })
         }
-        await data.save()
-        res.json({ message: 'user created', data: data })
+        await newUser.save()
+        res.json({ message: 'user created', data: newUser })
     } catch (error) {
         res.status(500).json({ message: 'Error to create new user' })
     }
@@ -40,15 +67,41 @@ exports.createUser = async (req, res, next) => {
 //update user
 exports.updateUser = async (req, res, next) => {
     try {
-        const data = await DataUsers.findOneAndUpdate({ _id: req.params.id }, req.body, {
-            new: true,
-            runvalidators: true,
-            context: 'query'
-        })
-        if (!data) {
+        const { id } = req.params
+        const updates = req.body
+        let updateduser
+
+        const user = await DataUsers.findById(id)
+        if (!user) {
             return res.status(400).json({ message: 'user not found' })
         }
-        res.json({ message: 'user updated', data: data })
+        switch (user.role) {
+            case 'student':
+                updateduser = await DataStudents.findByIdAndUpdate(id, updates, {
+                    new: true,
+                    runvalidators: true,
+                    context: 'query'
+                })
+                break;
+            case 'teacher':
+                updateduser = await DataTeachers.findByIdAndUpdate(id, updates, {
+                    new: true,
+                    runvalidators: true,
+                    context: 'query'
+                })
+                break;
+            case 'admin':
+                updateduser = await DataUsers.findByIdAndUpdate(id, updates, {
+                    new: true,
+                    runvalidators: true,
+                    context: 'query'
+                })
+                break;
+            default:
+                return res.status(400).json({ message: 'role doesnt exist' })
+        }
+
+        res.json({ message: 'user updated', data: updateduser })
 
     } catch (error) {
         res.status(500).json({ message: 'Error to update user' })
@@ -76,23 +129,23 @@ exports.deleteUser = async (req, res, next) => {
 
 exports.loginUser = async (req, res, next) => {
     const { user, password } = req.body
-    const fonduser = await DataUsers.findOne({ user: user, password: password })
+    const founduser = await DataUsers.findOne({ user: user, password: password })
 
-    if (fonduser) {
+    if (founduser) {
         const tokenPayload = {
-            userID: fonduser._id,
-            userEmail: fonduser.email,
-            useruser: fonduser.user,
+            userID: founduser._id,
+            userEmail: founduser.email,
+            useruser: founduser.user,
         }
         const token = generateToken(tokenPayload)
         const tokenStored = new TokenSchema({
             token: token,
-            userId: fonduser._id
+            userId: founduser._id
         })
-        fonduser.activity = true //user is loged
-        await fonduser.save()
+        founduser.activity = true //user is loged
+        await founduser.save()
         await tokenStored.save()
-        res.json({ success: true, message: 'user loged', user: fonduser.user, token: token })
+        res.json({ success: true, message: 'user loged', user: founduser.user, token: token })
     } else {
         res.status(400).json({ message: 'user not found', success: false })
     }
--- src/models/UserSchema.js
+++ src/models/UserSchema.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const options = { discriminatorKey: 'role', timestamps: true };
+
 const UserSchema = new Schema({
     user: {
         type: String,
@@ -14,15 +16,15 @@ const UserSchema = new Schema({
         type: String,
         required: true
     },
-    rol: {
-        type: String,
-        required: true
-    },
-    activity:{
+    activity: {
         type: Boolean,
         required: true,
         default: false
+    },
+    role: {
+        type: String,
+        required: true
     }
-}, { timestamps: true });
+}, options);
 
 module.exports = mongoose.model('User', UserSchema, 'users');
